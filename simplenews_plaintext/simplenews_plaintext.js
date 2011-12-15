4b39ca0640bbc3859336cb641926ba34949b3c95
Sascha Grossenbacher <sascha.grossenbacher@md-systems.ch>
2011-12-15 17:30:03 +0100

    further cleanup and start working on plaintext module.
    Conflicts:
    
    	simplenews.module

--- simplenews_plaintext/simplenews_plaintext.js
+++ simplenews_plaintext/simplenews_plaintext.js
@@ -1,9 +1,11 @@
 (function ($) {
 
-	// Defines an ajax command to replace content inside 
-	// the plaintext field after serverside processing
-	Drupal.ajax.prototype.commands.replacecontent = function (ajax, response, status) {
-	  $('#' + response.trigger).parent().find('textarea').val(response.plaintext);
-	}
+  // Defines an ajax command to replace content inside
+  // the plaintext field after serverside processing
+  Drupal.ajax.prototype.commands.replacecontent = function (ajax, response, status) {
+    console.log(response);
+    console.log($('#' + response.trigger).parent());
+    $('#' + response.trigger).parent().find('textarea').val(response.plaintext);
+  }
 
 })(jQuery);
