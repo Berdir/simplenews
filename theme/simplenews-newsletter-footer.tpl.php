be2e2b4b098108a286f671e1c9012f8c16df0ab4
Sascha Grossenbacher <saschagros@gmail.com>
2011-12-24 02:05:09 +0100

    move stuff, remove stuff, rename stuff.

--- theme/simplenews-newsletter-footer.tpl.php
+++ theme/simplenews-newsletter-footer.tpl.php
@@ -3,9 +3,9 @@
 /**
  * @file
  * Default theme implementation to format the simplenews newsletter footer.
- * 
+ *
  * Copy this file in your theme directory to create a custom themed footer.
- * Rename it to simplenews-newsletter-footer--<tid>.tpl.php to override it for a 
+ * Rename it to simplenews-newsletter-footer--<tid>.tpl.php to override it for a
  * newsletter using the newsletter term's id.
  *
  * @todo Update the available variables.
@@ -23,7 +23,6 @@
  * and 'simplenews-list'
  *
  * @see template_preprocess_simplenews_newsletter_footer()
- * @see theme_simplenews_newsletter_footer()
  */
 ?>
 <?php if ($format == 'html'): ?>
